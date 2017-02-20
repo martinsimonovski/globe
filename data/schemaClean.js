6c234974ca299a884a588f198131715c4fb54adf
Martin Simonovski <simonovski.martin@gmail.com>
2017-02-21 00:36:28 +0100

    working api - getting all elements

--- data/schemaClean.js
+++ data/schemaClean.js
@@ -1,14 +1,14 @@
-import { graphql, GraphQLSchema, GraphQLObjectType, GraphQLInt, GraphQLID, GraphQLString, GraphQLList } from 'graphql';
-import sqlite from 'sqlite3';
-import { promisify } from 'bluebird';
+import { graphql,
+         GraphQLSchema,
+         GraphQLObjectType,
+         GraphQLInt,
+         GraphQLID,
+         GraphQLString,
+         GraphQLList,
+         GraphQLNonNull,
+         GraphQLBoolean } from 'graphql';
 
-sqlite.verbose();
-const db = new sqlite.Database('data/globe.sqlite3');
-
-db.get = promisify(db.get);
-db.all = promisify(db.all);
-
-const ContinentType = new GraphQLObjectType({
+const Continent = new GraphQLObjectType({
   name: 'Continent',
   description: '...',
 
@@ -22,6 +22,7 @@ const ContinentType = new GraphQLObjectType({
   })
 });
 
+/*
 const QueryType = new GraphQLObjectType({
   name: 'Query',
   description: '...',
@@ -40,7 +41,82 @@ const QueryType = new GraphQLObjectType({
     }
   })
 });
-
-export default new GraphQLSchema({
-  query: QueryType
+*/
+const PageInfo = new GraphQLObjectType({
+  name: 'PageInfo',
+  fields: {
+    hasNextPage: {
+      type: new GraphQLNonNull(GraphQLBoolean),
+    },
+    hasPreviousPage: {
+      type: new GraphQLNonNull(GraphQLBoolean),
+    },
+  },
 });
+
+const ContinentConnection = new GraphQLObjectType({
+  name: 'ContinentConnection',
+  fields: () => ({
+    edges: {
+      type: new GraphQLList(ContinentEdge),
+      resolve(parent) {
+        return parent.query.then((result) => result);
+      },
+    },
+    pageInfo: {
+      type: new GraphQLNonNull(PageInfo),
+    },
+  }),
+});
+
+const ContinentEdge = new GraphQLObjectType({
+  name: 'ContinentEdge',
+  fields: () => ({
+    cursor: {
+      type: GraphQLString,
+    },
+    node: {
+      type: Continent,
+      resolve(parent) {
+        return parent;
+      }
+    },
+  }),
+});
+
+const Viewer = new GraphQLObjectType({
+  name: 'Viewer',
+  fields: () => ({
+    id: {
+      type: new GraphQLNonNull(GraphQLID),
+    },
+    allContinents: {
+      type: ContinentConnection,
+      resolve(parent, args, {db} ){
+        return {
+          query: db.all(`
+            SELECT * FROM Continent
+          `)
+        };
+      },
+    },
+  }),
+});
+
+const Schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+      viewer: {
+        type: Viewer,
+        resolve() {
+          return {
+            id: 'VIEWER_ID',
+          };
+        },
+      },
+    },
+  }),
+});
+
+export default Schema;
--- server.js
+++ server.js
@@ -4,12 +4,23 @@ import { addMockFunctionsToSchema } from 'graphql-tools';
 import bodyParser from 'body-parser';
 import schema from './data/schemaClean';
 
+import sqlite from 'sqlite3';
+import { promisify } from 'bluebird';
+
+sqlite.verbose();
+const db = new sqlite.Database('data/globe.sqlite3');
+
+db.get = promisify(db.get);
+db.all = promisify(db.all);
+
 const PORT = 8080;
 const app = express();
 
 app.use('/graphql', bodyParser.json(), graphqlExpress({
   schema: schema,
-  context: {},
+  context: {
+    db: db,
+  },
 })).use('/graphiql', graphiqlExpress({
   endpointURL: '/graphql',
 }));
